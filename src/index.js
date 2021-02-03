e304d78709aee33a195ce209881c0e3a05a40f27
William Addesso <wsaddesso@gmail.com>
2021-02-02 23:13:02 -0500

    In page-load.js, created an h1 element with restaurant name, a p element with a summary of restaurant, and an image element with an image of the restaurant.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { pageLoad } from './page-load'
 
 pageLoad();
+
--- src/page-load.js
+++ src/page-load.js
@@ -1,6 +1,19 @@
 function pageLoad() {
-    const container = document.getElementById("content");
-    console.log("tester");
+    const mainContainer = document.getElementById("content");
+
+    const restaurantTitle = document.createElement("h1");
+    restaurantTitle.id = "restaurantTitle";
+    restaurantTitle.innerHTML = "The Cantina";
+    mainContainer.appendChild(restaurantTitle);
+
+    const quickAbout = document.createElement("p");
+    quickAbout.id = "quickAbout";
+    quickAbout.innerHTML = "The best food and drinks this side of the galaxy";
+    mainContainer.appendChild(quickAbout);
+
+    const restaurantImage = document.createElement("img");
+    restaurantImage.src = "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/630/354/75/dam/disneyland/destinations/disneyland/star-wars-galaxys-edge/photography/ogas-cantina-16x9.jpg?1591980523666";
+    mainContainer.appendChild(restaurantImage);
 }
 
 export { pageLoad }
