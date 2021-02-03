429172ce631734a2852e2993bfcf60df47406652
William Addesso <wsaddesso@gmail.com>
2021-02-02 22:05:43 -0500

    Set image as background and positioned it to be centered. Began pageLoad function. Tested exporting pageLoad and importing to index.js to test functionality.

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.getElementById("content"),console.log("tester")})();
---
+++ src/page-load.js
@@ -0,0 +1,6 @@
+function pageLoad() {
+    const container = document.getElementById("content");
+    console.log("tester");
+}
+
+export { pageLoad }
