6c6fe9cd09da655c9c1e078bb64d559d45a074da
William Addesso <wsaddesso@gmail.com>
2021-02-04 14:12:31 -0500

    Refactored page-load.js to use functions to create elements in document. Began creating nav bar.

--- src/page-load.js
+++ src/page-load.js
@@ -2,61 +2,65 @@ function pageLoad() {
     // Main page div element
     const mainContainer = document.getElementById("content");
 
-    //  Header element.
-    const header = document.createElement("header");
-    header.id = "pageHeader";
-    mainContainer.appendChild(header);
+    function makeElement(id, type, parentEle) {
+        let tempEle = document.createElement(type);
+        tempEle.id = id;
+        parentEle.appendChild(tempEle)
+    }
 
+    function makeElementText(id, type, parentEle, text) {
+        let tempTextEle = document.createElement(type);
+        tempTextEle.id = id;
+        tempTextEle.innerHTML = text;
+        parentEle.appendChild(tempTextEle);
+    }
 
-    // Information section element
-    const infoSection = document.createElement("section");
-    infoSection.id = "infoSection";
-    mainContainer.appendChild(infoSection);
+    // Header element
+    makeElement("pageHeader", "header", mainContainer);
+    let pageHeader = document.getElementById("pageHeader")
+
+    // Information section element.
+    makeElement("infoSection", "section", mainContainer);
 
     // Footer element.
-    const footer = document.createElement("footer");
-    footer.id = "pageFooter";
-    mainContainer.appendChild(footer);
+    makeElement("pageFooter", "footer", mainContainer);
+    let pageFooter = document.getElementById("pageFooter");
 
     // Restaurant name element for header.
-    const name = document.createElement("h1");
-    name.id = "name";
-    name.innerHTML = "My Restaurant";
-    header.appendChild(name);
+    makeElementText("name", "h1", pageHeader, "My Restaurant");
 
     // Hours title element for footer
-    const hoursTitle = document.createElement("h3");
-    hoursTitle.id = "hoursTitle";
-    hoursTitle.innerHTML = "Hours";
-    footer.appendChild(hoursTitle);
-
-    // Hours text
-    const hoursText = document.createElement("p");
-    hoursText.id = "hoursText";
-    hoursText.innerHTML = `Tues - Sat: 11:30 am - 10:00 pm </br>
+    makeElementText("hoursTitle", "h3", pageFooter, "Hours");
+    
+    // Hours Text
+    makeElementText("hoursText", "p", pageFooter,
+        `Tues - Sat: 11:30 am - 10:00 pm </br>
         Sunday: 3:00 pm - 9:30 pm</br>
-        Monday: Closed`;
-    footer.appendChild(hoursText);
+        Monday: Closed`);
 
     // Contact title for footer
-    const contactTitle = document.createElement("h3");
-    contactTitle.id = "contactTitle"
-    contactTitle.innerHTML = "Contact"
-    footer.appendChild(contactTitle);
-
+    makeElementText("contactTitle", "h3", pageFooter, "Contact");
+    
     // Contact text
-    const contactText = document.createElement("p");
-    contactText.id = "contactText";
-    contactText.innerHTML = `914-555-5555</br>
+    makeElementText("contactText", "p", pageFooter,
+        `914-555-5555</br>
         inro@myrestaurant.com</br>
-        724 Cambridge Place, Faketown, PA`
-    footer.appendChild(contactText);
+        724 Cambridge Place, Faketown, PA`)
 
     // Nav bar
-    const navTabs = document.createElement("nav");
-    navTabs.id = "navTabs";
-    header.appendChild(navTabs);
+    makeElement("navTabs", "nav", pageHeader);
+    let navBar = document.getElementById("navTabs");
 
+    // Nav bar buttons list
+    const navList = document.createElement("ul");
+    navList.id = "navList";
+    navBar.appendChild(navList);
+    
+    // Home button
+    makeElementText("homeButton", "li", navList, 
+        `<a href="">Home</a>`)
+    
+    /*
     // Navbar tabs
     const homeTab = document.createElement("a");
     homeTab.id = "homeTab";
@@ -73,8 +77,10 @@ function pageLoad() {
     aboutTab.id = "aboutTab";
     aboutTab.innerHTML = "About";
     navTabs.appendChild(aboutTab);
-
+    */
     
 }
+
+
 
 export { pageLoad }
