3051eb9c04c3f6c31f814a11d4409ee088bf0978
William Addesso <wsaddesso@gmail.com>
2021-02-10 15:27:28 -0500

    Created menu.js functions to create elements for each menu section and item. Began adding menu items and sections.

--- src/menu.js
+++ src/menu.js
@@ -3,20 +3,52 @@ function menu() {
     const infoContainer = document.getElementById("infoSection")
 
     // A div element to contain all menu information.
-    const menuInfo = document.createElement("div");
+    const menuInfo = document.createElement("h4");
     menuInfo.id = "menuInfo";
-    menuInfo.innerHTML = "This is the menu";
+    menuInfo.innerHTML = "Menu";
     infoContainer.appendChild(menuInfo);
 
     // Function creates titles for menu sections
-
+    function createMenuHeader(id, className, parent, text) {
+        let newEle = document.createElement("h5");
+        newEle.id = id;
+        newEle.className = className;
+        parent.appendChild(newEle);
+        newEle.innerHTML = text;
+    }
     
 
     // Function creates a menu item.
+    function createMenuItem(className, parent, text) {
+        let newItem = document.createElement("p");
+        newItem.className = className;
+        parent.appendChild(newItem);
+        newItem.innerHTML = text;
+        newItem.style.fontWeight = 300;
+    }
+    
+    // Menu tab section "Starters".
+    createMenuHeader("starters", "menuSection", infoContainer, "Starters");
+    let startersSection = document.getElementById("starters");
 
+    // Starters section items.
+    createMenuItem("foodItem", startersSection, 
+    `-- Chicken Tenders -- $13</br>
+    <em>Spiced to perfection</br>
+    served with siracha slaw</em>.`
+    );
 
+    createMenuItem("foodItem", startersSection, 
+    `-- Nachos -- $13</br>
+    <em>Spiced to perfection</br>
+    served with siracha slaw</em>.`
+    );
 
-
+    createMenuItem("foodItem", startersSection, 
+    `-- Chicken Tenders -- $13</br>
+    <em>Spiced to perfection</br>
+    served with siracha slaw</em>.`
+    );
 
 }
 
